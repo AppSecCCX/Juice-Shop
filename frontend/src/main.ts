c083a58e6ec2525abbbf60e1c6e5a4d03a2a7afb
fadzayiChiwandire <124968942+AppSecCCX@users.noreply.github.com>
2024-01-19 04:01:44 +0000

    Update main.ts

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -16,4 +16,4 @@ if (environment.production) {
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch((err: Error) => console.log(err))
 
-  const password = 123456
+  const password = 1234569
